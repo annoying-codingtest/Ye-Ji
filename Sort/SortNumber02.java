367f8aa1bf57249daf404a5666afc2a64a76ab46
YJ-S2 <yegi0716@naver.com>
2021-09-18 19:37:12 +0900

    Sort-02 ADD

--- Sort/SortNumber02.java
+++ Sort/SortNumber02.java
@@ -1,3 +1,25 @@
+import java.util.*;
+
 public class SortNumber02 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int num = scan.nextInt();
+        StringBuilder s = new StringBuilder(); //효율을 위해
+
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        for(int i = 0; i < num; i++){
+            list.add(scan.nextInt());
+        }
+        
+        Collections.sort(list);
+
+        for(int i : list){
+             s.append(i).append('\n');
+        }
+        System.out.println(s);
     
+        scan.close();
+    }
 }
